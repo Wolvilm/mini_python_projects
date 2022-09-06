530b0548c7d209ff10fc777997d49185e6be24f4
Josh Dasi <joshuadasi@yahoo.com>
2022-09-06 04:25:18 -0400

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -10,37 +10,64 @@ def caesar(word, jump, action):
     if action == 'encode':
         cipher_text = ""
 
-        for letter in word:
+        # assigning variable for neatness
+        number_of_letters = len(alphabet)
 
-            # Identify the position of the letter in the list of alphabets.
-            position = alphabet.index(letter)
-            # Determine the new index location after adding the shift number.
-            new_position = position + jump
-            number_of_letters = len(alphabet)
+        # watching out for situation where shift is greater than number of alphabet letters.
 
-            # Dealing with a situation where we have to go past z and start over.
-            if new_position > number_of_letters - 1:
-                # Giving us how many places we should count from the beginning of the alphabet list.
-                new_position -= number_of_letters
-                new_letter = alphabet[new_position]
-                cipher_text += new_letter
+        for char in word:
+            if char in alphabet:
+                # Identify the position of the letter in the list of alphabets.
+                position = alphabet.index(char)
 
+                if jump > len(alphabet):
+                    new_position = position + (jump % len(alphabet))
+                else:
+                    new_position = position + jump
+
+                # Dealing with a situation where we have to go past z and start over.
+                if new_position > number_of_letters - 1:
+                    # Giving us how many places we should count from the beginning of the alphabet list.
+                    new_position -= number_of_letters
+                    new_letter = alphabet[new_position]
+                    cipher_text += new_letter
+
+                else:
+                    new_letter = alphabet[new_position]
+                    cipher_text += new_letter
+
+            # Takes care of characters not in alphabet
             else:
-                new_letter = alphabet[new_position]
-                cipher_text += new_letter
+                cipher_text += char
 
         print("The encoded output is {}".format(cipher_text))
 
-    if direction == 'decode':
+    if action == 'decode':
         decrypted_text = ""
 
-        for letter in word:
-            position = alphabet.index(letter)
-            new_position = position - jump
-            new_letter = alphabet[new_position]
-            decrypted_text += new_letter
+        for char in word:
+            if char in alphabet:
+                position = alphabet.index(char)
+
+                # handling case where shift is greater than number of letters in alphabet.
+                if jump > len(alphabet):
+                    new_position = position - (jump % len(alphabet))
+                else:
+                    new_position = position - jump
+
+                new_letter = alphabet[new_position]
+                decrypted_text += new_letter
 
         print("The decoded output is {}".format(decrypted_text))
 
 
 caesar(word=text, jump=shift, action=direction)
+
+run_again = (input("Would you like to run again? Type 'yes' or 'no': ")).lower()
+
+while run_again == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(word=text, jump=shift, action=direction)
+    run_again = (input("Would you like to run again?. Type 'yes to continue or anything else to end: ")).lower()
